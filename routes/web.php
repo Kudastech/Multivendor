ed0d1a44c5ab0ea16a5a2e14de12aed90daf092f
Kudafoke Isaiah Semeton <150586151+Kudastech@users.noreply.github.com>
2024-02-24 16:44:17 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,7 @@ Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function
         Route::get('categories', 'CategoryController@categories');
         Route::post('update-category-status', 'CategoryController@updateCategoryStatus');
         Route::match(['get', 'post'], 'add-edit-category/{id?}', 'CategoryController@addEditCategory');
+        Route::get('append-categories-level','CategoryController@appendCategoryLevel');
     });
 });
 
